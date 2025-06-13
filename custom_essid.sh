4bf13aeada9e5293ad7d2d6cdddf86f6ab454a39
OscarAkaElvis <oscar.alfonso.diaz@gmail.com>
2025-06-13 11:08:52 +0200

    Adapt custom_essid plugin to the first version supporting the hookable_for_languages function

--- custom_essid.sh
+++ custom_essid.sh
@@ -10,7 +10,7 @@ plugin_author="xpz3"
 #Enabled 1 / Disabled 0 - Set this plugin as enabled - Default value 1
 plugin_enabled=1
 
-plugin_minimum_ag_affected_version="10.0"
+plugin_minimum_ag_affected_version="11.41"
 plugin_maximum_ag_affected_version=""
 
 plugin_distros_supported=("*")
@@ -25,24 +25,21 @@ function custom_essid_prehook_set_hostapd_config() {
 	fi
 }
 
-function initialize_custom_essid_language_strings() {
+#Prehook for hookable_for_languages function to modify language strings
+#shellcheck disable=SC1111
+function custom_essid_prehook_hookable_for_languages() {
 
-	debug_print
-	
-	arr["ENGLISH","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["SPANISH","custom_essid_text_1"]="Una red oculta ha sido seleccionada para el ataque Evil Twin. Por favor especifica el SSID del AP objetivo para continuar"
-	arr["FRENCH","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["CATALAN","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["PORTUGUESE","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["RUSSIAN","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["GREEK","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["ITALIAN","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["POLISH","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["GERMAN","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["TURKISH","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["ARABIC","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the SSID of the target AP to continue"
-	arr["CHINESE","custom_essid_text_1"]="您选择了一个隐藏的网络发起邪恶双胞胎攻击，请手动指定目标隐藏AP的SSID以继续"
+	arr["ENGLISH","custom_essid_text_1"]="A hidden network has been chosen for Evil Twin attack. Please specify the ESSID of the target AP to continue"
+	arr["SPANISH","custom_essid_text_1"]="Se ha seleccionado una red oculta para el ataque Evil Twin. Por favor especifica el ESSID del AP objetivo para continuar"
+	arr["FRENCH","custom_essid_text_1"]="\${pending_of_translation} Un réseau caché pour l'attaque twin diabolique a été sélectionné. Veuillez spécifier l'ESSID de l'objectif AP à continuer"
+	arr["CATALAN","custom_essid_text_1"]="\${pending_of_translation} S'ha seleccionat una xarxa oculta per a l'atac bessó malvat. Especifiqueu l'ESSID de l'AP Objectiu per continuar"
+	arr["PORTUGUESE","custom_essid_text_1"]="\${pending_of_translation} Uma rede oculta para o ataque gêmeo do mal foi selecionada. Especifique o ESSID do objetivo AP para continuar"
+	arr["RUSSIAN","custom_essid_text_1"]="\${pending_of_translation} Была выбрана скрытая сеть для злой атаки близнецов. Пожалуйста, укажите ESSID цели AP для продолжения"
+	arr["GREEK","custom_essid_text_1"]="\${pending_of_translation} Έχει επιλεγεί ένα κρυφό δίκτυο για την κακή δίδυμη επίθεση. Προσδιορίστε το ESSID του αντικειμενικού AP να συνεχιστεί"
+	arr["ITALIAN","custom_essid_text_1"]="\${pending_of_translation} È stata selezionata una rete nascosta per l'attacco gemello malvagio. Si prega di specificare l'ESSID dell'obiettivo AP per continuare"
+	arr["POLISH","custom_essid_text_1"]="\${pending_of_translation} Wybrano ukrytą sieć złego ataku bliźniaczego. Podaj ESSID obiektywnego AP, aby kontynuować"
+	arr["GERMAN","custom_essid_text_1"]="\${pending_of_translation} Ein verstecktes Netzwerk für den bösen Twin -Angriff wurde ausgewählt. Bitte geben Sie den ESSID des objektiven AP an, um fortzufahren"
+	arr["TURKISH","custom_essid_text_1"]="\${pending_of_translation} Kötü ikiz saldırı için gizli bir ağ seçildi. Devam etmek için lütfen nesnel AP'nin ESSIDini belirtin"
+	arr["ARABIC","custom_essid_text_1"]="\${pending_of_translation} تم اختيار شبكة خفية للهجوم التوأم الشرير. يرجى تحديد ESSID من الهدف AP للمتابعة"
+	arr["CHINESE","custom_essid_text_1"]="\${pending_of_translation} 已经选择了一个邪恶双胞胎攻击的隐藏网络。请指定目标AP的ESSID"
 }
-
-initialize_custom_essid_language_strings
-
